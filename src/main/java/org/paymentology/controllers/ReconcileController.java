6644f1363650bb55c4d0621d6a7d3e6cc0e50c5d
andre-macedo-matos <andremacedomatos@gmail.com>
2022-05-18 15:09:52 -0300

    Build link to expose report as json

--- src/main/java/org/paymentology/controllers/ReconcileController.java
+++ src/main/java/org/paymentology/controllers/ReconcileController.java
@@ -6,6 +6,7 @@ import org.paymentology.models.Inputs;
 import org.paymentology.models.Report;
 import org.paymentology.validators.InputsValidator;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
@@ -14,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 @Scope(value = WebApplicationContext.SCOPE_SESSION)
@@ -52,9 +56,28 @@ public class ReconcileController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView form(Inputs inputs) {
+		this.report = new Report(inputs);
+		
 		ModelAndView modelAndView = new ModelAndView("form");
 		modelAndView.addObject("showCompare", "display:none;");
 		modelAndView.addObject("showTable", "display:none;");
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/json/report", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ModelAndView jsonReport(@Valid Inputs inputs) {
+		ModelAndView modelAndView = new ModelAndView("report");
+		Report report = new Report(inputs);
+		
+		String reportAsJson;
+		try {
+			reportAsJson = new ObjectMapper().writeValueAsString(report);
+		} catch (JsonProcessingException e) {
+			reportAsJson = "Error to convert to json: " + e.getMessage();
+			e.printStackTrace();
+		}
+		modelAndView.addObject("report", reportAsJson);
+		
 		return modelAndView;
 	}
 
